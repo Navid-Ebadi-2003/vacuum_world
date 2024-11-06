9881518f023bfa09d9445ee3ae278613d4a96c39
navid ebadi <navid.ebadi.2003@gmail.com>
2024-11-06 18:06:15 +0330

    Start of the project

---
+++ agents.py
@@ -0,0 +1,89 @@
+class agent:
+    pass
+
+
+class ag_fullyObs_deterministic_static(agent):
+    pass
+
+
+class ag_fullyObs_deterministic_dynamic(agent):
+    pass
+
+
+
+class ag_fullyObs_stochasticInMove_static(agent):
+    pass
+
+
+class ag_fullyObs_stochasticInMove_dynamic(agent):
+    pass
+
+
+
+class ag_fullyObs_stochasticInVac_static(agent):
+    pass
+
+
+class ag_fullyObs_stochasticInVac_dynamic(agent):
+    pass
+
+
+
+
+
+
+
+class ag_noPositionSensor_deterministic_static(agent):
+    pass
+
+
+class ag_noPositionSensor_deterministic_dynamic(agent):
+    pass
+
+
+
+class ag_noPositionSensor_stochasticInMove_static(agent):
+    pass
+
+
+class ag_noPositionSensor_stochasticInMove_dynamic(agent):
+    pass
+
+
+
+class ag_noPositionSensor_stochasticInVac_static(agent):
+    pass
+
+
+class ag_noPositionSensor_stochasticInVac_dynamic(agent):
+    pass
+
+
+
+
+
+
+class ag_noCleanSensor_deterministic_static(agent):
+    pass
+
+
+class ag_noCleanSensor_deterministic_dynamic(agent):
+    pass
+
+
+
+class ag_noCleanSensor_stochasticInMove_static(agent):
+    pass
+
+
+class ag_noCleanSensor_stochasticInMove_dynamic(agent):
+    pass
+
+
+
+class ag_noCleanSensor_stochasticInVac_static(agent):
+    pass
+
+
+class ag_noCleanSensor_stochasticInVac_dynamic(agent):
+    pass
---
+++ environments.py
@@ -0,0 +1,74 @@
+class environment:
+    pass
+
+
+class env_fullyObs_deterministic_static(environment):
+    pass
+
+
+class env_fullyObs_deterministic_dynamic(environment):
+    pass
+
+
+class env_fullyObs_stochasticInMove_static(environment):
+    pass
+
+
+class env_fullyObs_stochasticInMove_dynamic(environment):
+    pass
+
+
+class env_fullyObs_stochasticInVac_static(environment):
+    pass
+
+
+class env_fullyObs_stochasticInVac_dynamic(environment):
+    pass
+
+
+class env_noPositionSensor_deterministic_static(environment):
+    pass
+
+
+class env_noPositionSensor_deterministic_dynamic(environment):
+    pass
+
+
+class env_noPositionSensor_stochasticInMove_static(environment):
+    pass
+
+
+class env_noPositionSensor_stochasticInMove_dynamic(environment):
+    pass
+
+
+class env_noPositionSensor_stochasticInVac_static(environment):
+    pass
+
+
+class env_noPositionSensor_stochasticInVac_dynamic(environment):
+    pass
+
+
+class env_noCleanSensor_deterministic_static(environment):
+    pass
+
+
+class env_noCleanSensor_deterministic_dynamic(environment):
+    pass
+
+
+class env_noCleanSensor_stochasticInMove_static(environment):
+    pass
+
+
+class env_noCleanSensor_stochasticInMove_dynamic(environment):
+    pass
+
+
+class env_noCleanSensor_stochasticInVac_static(environment):
+    pass
+
+
+class env_noCleanSensor_stochasticInVac_dynamic(environment):
+    pass
