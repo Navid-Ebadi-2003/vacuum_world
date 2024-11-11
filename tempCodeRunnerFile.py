48c2fb9ed9f640b2103b04e097ac9d4d5ff0f5a8
navid ebadi <navid.ebadi.2003@gmail.com>
2024-11-11 23:14:53 +0330

    fix test bug

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,84 @@
+print("-------fullyObs_deterministic_dynamic-------")
+# env = environments.env_fullyObs_deterministic_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_fullyObs_deterministic_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------fullyObs_stochasticInMove_static-------")
+# env = environments.env_fullyObs_stochasticInMove_static("dirty", "clean", "dirty")
+# agent = agents.ag_fullyObs_stochasticInMove_static(env)
+# run_simulation(env, agent)
+
+# print("-------fullyObs_stochasticInMove_dynamic-------")
+# env = environments.env_fullyObs_stochasticInMove_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_fullyObs_stochasticInMove_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------fullyObs_stochasticInVac_static-------")
+# env = environments.env_fullyObs_stochasticInVac_static("dirty", "clean", "dirty")
+# agent = agents.ag_fullyObs_stochasticInVac_static(env)
+# run_simulation(env, agent)
+
+# print("-------fullyObs_stochasticInVac_dynamic-------")
+# env = environments.env_fullyObs_stochasticInVac_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_fullyObs_stochasticInVac_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_deterministic_static-------")
+# env = environments.env_noCleanSensor_deterministic_static("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_deterministic_static(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_deterministic_dynamic-------")
+# env = environments.env_noCleanSensor_deterministic_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_deterministic_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_stochasticInMove_static-------")
+# env = environments.env_noCleanSensor_stochasticInMove_static("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_stochasticInMove_static(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_stochasticInMove_dynamic-------")
+# env = environments.env_noCleanSensor_stochasticInMove_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_stochasticInMove_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_stochasticInVac_static-------")
+# env = environments.env_noCleanSensor_stochasticInVac_static("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_stochasticInVac_static(env)
+# run_simulation(env, agent)
+
+# print("-------noCleanSensor_stochasticInVac_dynamic-------")
+# env = environments.env_noCleanSensor_stochasticInVac_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noCleanSensor_stochasticInVac_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_deterministic_static-------")
+# env = environments.env_noPositionSensor_deterministic_static("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_deterministic_static(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_deterministic_dynamic-------")
+# env = environments.env_noPositionSensor_deterministic_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_deterministic_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_stochasticInMove_static-------")
+# env = environments.env_noPositionSensor_stochasticInMove_static("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_stochasticInMove_static(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_stochasticInMove_dynamic-------")
+# env = environments.env_noPositionSensor_stochasticInMove_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_stochasticInMove_dynamic(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_stochasticInVac_static-------")
+# env = environments.env_noPositionSensor_stochasticInVac_static("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_stochasticInVac_static(env)
+# run_simulation(env, agent)
+
+# print("-------noPositionSensor_stochasticInVac_dynamic-------")
+# env = environments.env_noPositionSensor_stochasticInVac_dynamic("dirty", "clean", "dirty")
+# agent = agents.ag_noPositionSensor_stochasticInVac_dynamic(env)
+# run_simulation(env, agent)
